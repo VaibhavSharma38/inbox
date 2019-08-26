d62b8c51c3633b7c6a7c23be2aa908bb64894f20
Vaibhav Sharma <vaibhavsharma38@gmail.com>
2019-08-26 23:20:43 +0530

    deployed contract to test network

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,29 +2,20 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile.js');
 
-class Car {
-	park() {
-		return 'Stopped';
-	}
- 
-	drive() {
-		return 'Vroom';
-	}
-}
+let accounts;
+let inbox;
 
-let car;
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
 
-beforeEach(() => {
-	car = new Car();
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({data : bytecode, arguments : ['Hi there!']})
+		.send({from : accounts[0], gas:1000000});
 });
 
-describe('Car', () => {
-	it('can park', () => {
-		assert.equal(car.park(), 'Stopped');	
-	});
-
-	it('can drive', () => {
-		assert.equal(car.drive(), 'Vroom');
+describe('Inbox', () => {
+	it('Deploys a contract', () => {
 	});
 });
